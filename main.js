a1e833608e46e1df23f6e08f87de51a93b0705cc
Drayko <no.replybot5455@gmail.com>
2025-12-24 13:05:26 +0100

    Refonte

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
+        minWidth: 800,
+        minHeight: 600,
         frame: false,
         titleBarStyle: 'hidden',
         backgroundColor: '#0f172a',
